1a30c0c447bd076fde56f9b8b4dc7b7cc5d41cb5
vituud2 <overroot0@gmail.com>
2026-05-26 11:39:31 -0300

    feat: add ColorInput component for selecting hex colors

--- src/components/color-input.tsx
+++ src/components/color-input.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useRef } from "react";
+
 type ColorInputProps = {
   value: string;
   label: string;
@@ -7,13 +9,26 @@ type ColorInputProps = {
 };
 
 export function ColorInput({ value, label, onChange }: ColorInputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <input
+    <button
+      type="button"
       aria-label={label}
-      type="color"
-      value={value}
-      onChange={(event) => onChange(event.target.value)}
-      className="h-7 w-8 cursor-pointer rounded border border-white/10 bg-transparent p-0 focus:outline-none focus:border-stone-300/40 hover:border-white/20 transition-all [&::-webkit-color-swatch-wrapper]:p-0.5 [&::-webkit-color-swatch]:border-none [&::-webkit-color-swatch]:rounded-[2px] [&::-moz-color-swatch]:border-none [&::-moz-color-swatch]:rounded-[2px]"
-    />
+      className="group relative flex h-7 w-8 cursor-pointer items-center justify-center rounded border border-white/10 bg-white/[0.035] transition hover:border-white/20 hover:bg-white/[0.06] focus:outline-none focus:border-stone-300/40"
+      onClick={() => inputRef.current?.click()}
+    >
+      <span className="pointer-events-none flex h-full w-full items-center justify-center">
+        <span className="block h-4 w-4 rounded-sm border border-black/20" style={{ backgroundColor: value }} />
+      </span>
+      <input
+        ref={inputRef}
+        className="absolute pointer-events-none opacity-0"
+        style={{ width: "1px", height: "1px", overflow: "hidden", left: "0", top: "0" }}
+        type="color"
+        value={value}
+        onChange={(event) => onChange(event.target.value)}
+      />
+    </button>
   );
 }
